e3b73c2f426fad159c8d5638c6e6a723cd0865bb
Valeriy Shesternin <serotonin.pepperonin@gmail.com>
2025-03-07 12:52:02 +0500

    2 - 3 Подготовка проекта

---
+++ next.config.ts
@@ -0,0 +1,42 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: { not: /\.(css|scss|sass)$/ },
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        loader: "@svgr/webpack",
+        options: {
+          dimensions: false,
+          titleProp: true,
+        },
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  images: {
+    domains: ["ratingus.hb.bizmrg.com"],
+  },
+};
+
+export default nextConfig;
